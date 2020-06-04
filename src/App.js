75c06c42c3782ad0c70b5c7dbfb1c5779b82bb23
Hunter Danielson <hunterleedanielson@gmail.com>
2020-06-03 20:50:51 -0700

    data in place

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import $ from 'jquery';
 import './App.css';
 import data from './resumeData.json'
 import Header from './Components/Header';
@@ -7,7 +6,7 @@ import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
 import Contact from './Components/Contact';
-import Testimonials from './Components/Testimonials';
+// import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
 
 
@@ -38,7 +37,7 @@ class App extends Component {
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
-        <Testimonials data={this.state.resumeData.testimonials}/>
+        {/* <Testimonials data={this.state.resumeData.testimonials}/> */}
         <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
